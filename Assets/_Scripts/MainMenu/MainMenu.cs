df9fae2d81f296d442a8d3d2d4f9533b18141d47
Aryazaky <aryazakyazif@gmail.com>
2023-05-21 18:10:51 +0700

    Add custom logger

--- Assets/_Scripts/MainMenu/MainMenu.cs
+++ Assets/_Scripts/MainMenu/MainMenu.cs
@@ -3,23 +3,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Experimental.AI;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
-    // Obejct in this main menu groub
-    [SerializeField] private GameObject MainMenuPanel;
-    [SerializeField] private GameObject MainMenuButton;
+    // Object in this main menu group
+    [SerializeField] private RectTransform MainMenuPanel;
+    [SerializeField] private Button MainMenuButton;
 
-    [SerializeField] private GameObject MenuPanelGroup;
-    [SerializeField] private GameObject StatsPanel;
-    [SerializeField] private GameObject AchievementPanel;
-    [SerializeField] private GameObject DictionaryPanel;
+    [SerializeField] private RectTransform MenuPanelGroup;
+    [SerializeField] private RectTransform StatsPanel;
+    [SerializeField] private RectTransform AchievementPanel;
+    [SerializeField] private RectTransform DictionaryPanel;
 
     // Public Variable
     public const float Slide_Duration = 0.1f;
 
     private void Start()
     {
+        
         // Always close main menu first
         CloseMainMenu(0);
         // Got to stats panel first
@@ -27,7 +29,7 @@ public class MainMenu : MonoBehaviour
     }
 
     // Open Close Main Menu ---------------------------------------------------------------------------
-    private float MainMenuPanelHeight => MainMenuPanel.GetComponent<RectTransform>().rect.height;
+    private float MainMenuPanelHeight => MainMenuPanel.rect.height;
     private float MainMenuButtonHeight => MainMenuButton.GetComponent<RectTransform>().rect.height;
     private bool m_mainMenuIsOpen = true;
     public void OpenMainMenu(float duration =  Slide_Duration)
@@ -43,30 +45,37 @@ public class MainMenu : MonoBehaviour
     public void OpenCloseMainMenu(float duration)
     {
         m_mainMenuIsOpen = !m_mainMenuIsOpen;
-        
-        if(m_mainMenuIsOpen)
-            MainMenuPanel.transform.DOMoveY(MainMenuPanel.transform.position.y + (MainMenuPanelHeight * 2) - MainMenuButtonHeight, duration);
+        float targetY;
+
+        if (m_mainMenuIsOpen)
+        {
+            targetY = MainMenuPanel.position.y + (MainMenuPanelHeight * 2) - MainMenuButtonHeight;
+            MainMenuPanel.DOMoveY(targetY, duration);
+        }
         else
-            MainMenuPanel.transform.DOMoveY(MainMenuPanel.transform.position.y - (MainMenuPanelHeight * 2) + MainMenuButtonHeight, duration);
+        {
+            targetY = MainMenuPanel.position.y - (MainMenuPanelHeight * 2) + MainMenuButtonHeight;
+            MainMenuPanel.DOMoveY(targetY, duration);
+        }
     }
 
     // Slide Panel Menu -------------------------------------------------------------------------------
     private float SubMenuPanelWidth => StatsPanel.GetComponent<RectTransform>().rect.width;
     private const float SubMenuPanelMargin = 20.0f;
-    private int m_subMenupPanelIndex = 0;
+    private int m_subMenuPanelIndex = 0;
     public void GoToMenuPanel(int index)
     {
         // Checking index value
-        if (m_subMenupPanelIndex == index) return;
+        if (m_subMenuPanelIndex == index) return;
         if(index < 0 || index > 2)
         {
             index = 0;
-            if(m_subMenupPanelIndex == index)
+            if(m_subMenuPanelIndex == index)
             {
                 return;
             }
         }
-        m_subMenupPanelIndex = index;
+        m_subMenuPanelIndex = index;
         
         // Translate rectTransform to transform
         var xRectPos = -(SubMenuPanelWidth + SubMenuPanelMargin) * index;
