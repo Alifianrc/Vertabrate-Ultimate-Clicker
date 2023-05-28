8091c29d7f8e839c317d2f09edab20d7d2f2835c
Alifianrc <62532983+Alifianrc@users.noreply.github.com>
2023-05-28 11:47:01 +0700

    Update Stats Menu

--- Assets/_Scripts/MainMenu/StatsGroup.cs
+++ Assets/_Scripts/MainMenu/StatsGroup.cs
@@ -1,22 +1,77 @@
-using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class StatsGroup : MonoBehaviour
 {
     [SerializeField] private GameObject BoxGroup;
     [SerializeField] private Transform FirstStatsBoxPos;
     [SerializeField] private GameObject StatsBoxPrefab;
+    [SerializeField] private GameObject UpgradeButton;
+    [SerializeField] private TextMeshProUGUI UpgradeCost;
 
+    [SerializeField] private List<string> BoxesHexColor;
+
+    public const int BoxesCount = 10;
     public const float StatsBoxMargin = 15.0f;
+
+    public Image[] Boxes { get; private set; }
+
+    public int MainLevelsCount { get; set; } = 0;
+    public int SubLevelCount { get; set; } = 0;
 
     private void Start()
     {
-        for (int i = 0; i < 10; i++)
+        Boxes = new Image[BoxesCount];
+        for (int i = 0; i < BoxesCount; i++)
         {
+            // Create boxes
             var newBox = Instantiate(StatsBoxPrefab, BoxGroup.transform);
             var transPoint = (new Vector3(FirstStatsBoxPos.transform.position.x, FirstStatsBoxPos.transform.position.y, 0));
             newBox.transform.position = new Vector2(transPoint.x + ((newBox.GetComponent<RectTransform>().rect.width * 2 + StatsBoxMargin) * i), transPoint.y);
+
+            Boxes[i] = newBox.GetComponent<Image>();
+
+            // Set color
+            if (i < SubLevelCount) Boxes[i].color = ConvertHexToColor(BoxesHexColor[MainLevelsCount + 1]);
+            else Boxes[i].color = ConvertHexToColor(BoxesHexColor[MainLevelsCount]);
         }
+    }
+
+    public void UpdateBoxesColor()
+    {
+        for (int i = 0; i < BoxesCount; i++)
+        {
+            if (i < SubLevelCount) Boxes[i].color = ConvertHexToColor(BoxesHexColor[MainLevelsCount + 1]);
+            else Boxes[i].color = ConvertHexToColor(BoxesHexColor[MainLevelsCount]);
+        }
+    }
+
+    public void UpdateUpgradeCost(int value)
+    {
+        UpgradeCost.text = value.ToString();
+    }
+
+    public void IncreaseSubLevel()
+    {
+        SubLevelCount++;
+        if(SubLevelCount >= BoxesCount)
+        {
+            SubLevelCount = 0;
+            MainLevelsCount++;
+        }
+
+        // Update Color
+        UpdateBoxesColor();
+
+        Debug.Log("Sublevel " + SubLevelCount + " MainLevel " + MainLevelsCount);
+    }
+
+    private Color ConvertHexToColor(string hexColor)
+    {
+        ColorUtility.TryParseHtmlString(hexColor, out var color);
+        if(color == null) color = Color.white;
+        return color;
     }
 }
