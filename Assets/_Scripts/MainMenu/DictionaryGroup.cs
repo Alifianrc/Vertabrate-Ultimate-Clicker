3d91676f5298ed0c27028cfa7f9337cd6c621af1
Alifianrc <62532983+Alifianrc@users.noreply.github.com>
2023-05-28 12:23:14 +0700

    Create Desc Panel

---
+++ Assets/_Scripts/MainMenu/DictionaryGroup.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DictionaryGroup : MonoBehaviour
+{
+    [SerializeField] public GameObject DictionaryOne;
+    [SerializeField] public GameObject DictionaryTwo;
+    [SerializeField] public GameObject DictionaryThree;
+}
---
+++ Assets/_Scripts/MainMenu/DictionaryPanel.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DictionaryPanel : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI Title;
+    [SerializeField] private TextMeshProUGUI Subtitle;
+    [SerializeField] private Image Image;
+
+    [SerializeField] private TextMeshProUGUI DescText;
+
+
+}
