39c06e81910332f1abe02e0ec38701443c397c0a
Alifianrc <62532983+Alifianrc@users.noreply.github.com>
2023-05-28 11:51:47 +0700

    Update Achievement Group

---
+++ Assets/_Scripts/MainMenu/AchievementGroup.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class AchievementGroup : MonoBehaviour
+{
+    [SerializeField] private GameObject ClaimButton;
+    [SerializeField] private TextMeshProUGUI AchievementText;
+
+    public int RewardValue { get; private set; }
+
+    public void SetAchievementText(string text)
+    {
+        AchievementText.text = text;
+    }
+    public void SetRewardValue(int value) 
+    { 
+        RewardValue = value;
+    }
+}
